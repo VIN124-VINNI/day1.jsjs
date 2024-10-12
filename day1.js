8f5090b3c3aef9e636c14750226fb25938754c4e
VIN124-VINNI <vineelareddy925@gmail.com>
2024-10-12 15:07:35 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,4 @@
+var data = [10, 20, ["apple", "banana"], ["cat", ["dog", "elephant"], "fish"], "grape"];
+console.log(data[3][2])
+var items = [["pen", "pencil"], "notebook", [1, 2, [3, 4, ["eraser"]]], "marker"];
+console.log(items[2][2][2][0]);
